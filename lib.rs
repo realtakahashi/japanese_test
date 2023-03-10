2d5805b007e4ba704edd7b60d0b8aaf203dd45a0
shin.takahashi <realtakahashi.0924@gmail.com>
2023-03-10 17:44:44 +0900

    first commit

---
+++ lib.rs
@@ -0,0 +1,86 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[ink::contract]
+mod japanese_test {
+    use ink::prelude::string::{ String, ToString};
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+    #[ink(storage)]
+    pub struct JapaneseTest {
+        /// Stores a single `bool` value on the storage.
+        value: bool,
+        japanese_value: String,
+    }
+
+    impl JapaneseTest {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(init_value: bool) -> Self {
+            Self { value: init_value, japanese_value:"".to_string(), }
+        }
+
+        /// Constructor that initializes the `bool` value to `false`.
+        ///
+        /// Constructors can delegate to other constructors.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(Default::default())
+        }
+
+        /// A message that can be called on instantiated contracts.
+        /// This one flips the value of the stored `bool` from `true`
+        /// to `false` and vice versa.
+        #[ink(message)]
+        pub fn flip(&mut self) {
+            self.value = !self.value;
+        }
+
+        /// Simply returns the current value of our `bool`.
+        #[ink(message)]
+        pub fn get(&self) -> bool {
+            self.value
+        }
+
+        #[ink(message)]
+        pub fn set_japanese(&mut self, japanese_value:String) {
+            self.japanese_value = japanese_value;
+        }
+
+        #[ink(message)]
+        pub fn get_japanese_value(&self) -> String {
+            self.japanese_value.clone()
+        }
+
+        #[ink(message)]
+        pub fn get_japanese_const_value(&self) -> String {
+            "高橋です。".to_string()
+        }
+
+    }
+
+    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
+    /// module and test functions are marked with a `#[test]` attribute.
+    /// The below code is technically just normal Rust code.
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        /// We test if the default constructor does its job.
+        #[ink::test]
+        fn default_works() {
+            let japanese_test = JapaneseTest::default();
+            assert_eq!(japanese_test.get(), false);
+        }
+
+        /// We test a simple use case of our contract.
+        #[ink::test]
+        fn it_works() {
+            let mut japanese_test = JapaneseTest::new(false);
+            assert_eq!(japanese_test.get(), false);
+            japanese_test.flip();
+            assert_eq!(japanese_test.get(), true);
+        }
+    }
+}
